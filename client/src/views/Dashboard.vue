0bb31756157dc6b9ee1a11dd618d7deda46ebdeb
Gabriel Alves <l.luis.gabriel@outlook.com>
2019-11-28 18:28:07 -0300

    feat(frontend): register page created

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'dashboard',
+  data: () => ({
+    //
+  }),
+  computed: {
+    getApiUrl() {
+      return this.$store.getters.getApiUrl;
+    },
+  },
+  mounted() {
+    axios.get(`${this.getApiUrl}/users`, {
+      headers: {
+        token: localStorage.token,
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  },
 };
 </script>
 
--- client/src/views/LandingPage.vue
+++ client/src/views/LandingPage.vue
@@ -1,32 +1,80 @@
 <template>
   <div class="landingPage">
     <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            label="E-mail"
-            prepend-icon="mdi-account-circle"
-            v-model="user.email"
-          />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="Senha"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-            v-model="user.password"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Criar uma conta</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info" @click.prevent="login">{{buttonLogin}}</v-btn>
-      </v-card-actions>
+      <div v-if="toggleLogin">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="E-mail"
+              prepend-icon="mdi-account-circle"
+              v-model="user.email"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Senha"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+              v-model="user.password"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success" @click.prevent="toggleLogin = false">Criar uma conta</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info" @click.prevent="login">{{buttonLogin}}</v-btn>
+        </v-card-actions>
+      </div>
+      <div v-if="!toggleLogin">
+        <v-card-title>
+          <h1 class="display-1">Criar uma conta</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Nome"
+              prepend-icon="mdi-pencil"
+              v-model="user.email"
+            />
+            <v-text-field
+              label="E-mail"
+              prepend-icon="mdi-account-circle"
+              v-model="user.email"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Senha"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+              v-model="user.password"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Confirmar senha"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+              v-model="user.confirmPassword"
+            />
+            <v-select
+              prepend-icon="mdi-tennis"
+              :items="['Comprador', 'Vendedor']"
+              label="Tipo de conta"
+            ></v-select>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="info" @click.prevent="toggleLogin = true">Ja tenho uma conta</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="success" @click.prevent="register">Registrar</v-btn>
+        </v-card-actions>
+      </div>
     </v-card>
   </div>
 </template>
@@ -37,11 +85,16 @@ import axios from 'axios';
 export default {
   name: 'landingPage',
   data: () => ({
+    toggleLogin: true,
     buttonLogin: 'Logar',
     showPassword: false,
     user: {
+      name: '',
       email: '',
       password: '',
+      confirmPassword: '',
+      accountType: '',
+      isSeller: 0,
     },
   }),
   computed: {
@@ -64,6 +117,33 @@ export default {
       }).finally(() => {
         this.buttonLogin = 'Logar';
       });
+    },
+    validUser() {
+      if (this.user.password !== this.user.confirmPassword) {
+        alert('As senhas digitadas estão diferentes!');
+        return false;
+      }
+
+      if (this.user.accountType === 'Vendedor') this.user.isSeller = 1;
+
+      // Adicionar validação de usuario
+
+      return true;
+    },
+    register() {
+      if (this.validUser()) {
+        const body = {
+          name: this.user.name,
+          email: this.user.email,
+          password: this.user.password,
+          isSeller: this.user.isSeller,
+        };
+        axios.post(`${this.getApiUrl}/users`, body).then(() => {
+          alert('Conta cadastrada com sucesso, você já pode fazer o login!');
+        }).catch(() => {
+          alert('Esse e-mail já está cadastrado, tente fazer o login!');
+        });
+      }
     },
   },
 };
