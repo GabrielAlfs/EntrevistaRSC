15ff9314dd465a06724c0161560eee1490cce906
Gabriel Alves <l.luis.gabriel@outlook.com>
2019-11-28 21:18:03 -0300

    feat(frontend): added buy and sell

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="dashboard">
+    <v-overlay :value="overlay">
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Adicionar produto</h1>
+          <v-spacer></v-spacer>
+          <v-btn
+            icon
+            @click="overlay = false"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Nome do produto"
+              prepend-icon="mdi-pencil"
+              v-model="newProduct.name"
+            />
+            <v-text-field
+              type="number"
+              step="0.01"
+              label="Preço"
+              prepend-icon="mdi-cash"
+              v-model="newProduct.price"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" @click="add()">Adicionar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-overlay>
     <v-toolbar dense>
       <v-toolbar-title>Loja</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn icon v-if="getUser">
-        <v-icon v-if="getUser.isSeller === 1">mdi-plus</v-icon>
-      </v-btn>
-
+      <div v-if="getUser">
+        <v-btn icon v-if="getUser.isSeller === 1" @click="overlay = !overlay">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn color="red" text @click.prevent="logout">Sair</v-btn>
+      </div>
     </v-toolbar>
     <div class="d-flex flex-wrap justify-space-around">
       <v-card
@@ -40,6 +76,7 @@
           <v-btn
             color="success"
             text
+            @click.prevent="buy(product.id, index)"
           >
             Comprar
           </v-btn>
@@ -57,6 +94,11 @@ export default {
   name: 'dashboard',
   data: () => ({
     products: [],
+    overlay: false,
+    newProduct: {
+      name: '',
+      price: '',
+    },
   }),
   computed: {
     getApiUrl() {
@@ -67,6 +109,37 @@ export default {
     },
   },
   methods: {
+    removeFromArray(array, value) {
+      return array.filter(element => element.id !== value);
+    },
+    buy(productId, index) {
+      axios.delete(`${this.getApiUrl}/products/${productId}`, {
+        headers: {
+          token: localStorage.token,
+        },
+      }).then(() => {
+        this.products.splice(this.products.indexOf(this.products[index]), 1);
+        alert('Compra realizada com sucesso!');
+      });
+    },
+    add() {
+      this.overlay = false;
+      const data = {
+        name: this.newProduct.name,
+        price: Number(this.newProduct.price),
+        user_id: Number(this.getUser.sub),
+      };
+      axios.post(`${this.getApiUrl}/products`, data, {
+        headers: {
+          token: localStorage.token,
+        },
+      }).then((res) => {
+        this.products.push(res.data);
+        this.newProduct.name = '';
+        this.newProduct.price = '';
+        alert('Produto adicionado com sucesso!');
+      });
+    },
     logout() {
       localStorage.removeItem('token');
       this.$router.push('/');
